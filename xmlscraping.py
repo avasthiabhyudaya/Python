509f0bf6c39547071df981bd1cd62c533107bf9f
avasthiabhyudaya <avasthiabhyudaya@gmail.com>
2020-05-02 20:47:59 +0530

    XML_scraping
    This is the Assignment 2,Week 5 of Using Python to Access Web Data, where we need to using a url of an xml file to scrape the number from a subtag and find the sum of all those numbers

---
+++ xmlscraping.py
@@ -0,0 +1,19 @@
+import urllib.request, urllib.parse, urllib.error
+import re
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import ssl
+import xml.etree.ElementTree as ET
+a=list()
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+r=list()
+url = input('Enter - ')
+xml = urlopen(url, context=ctx).read()
+tree = ET.fromstring(xml)
+lst = tree.findall(".//count")
+for count in lst:
+    a.append(int(count.text))
+s = sum(a)
+print(s)
